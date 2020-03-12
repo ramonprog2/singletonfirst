ad3aeced0370f30c73a716540c91c94aa170c1fd
unknown <ramoncito.guerra14@gmail.com>
2020-03-12 12:29:51 -0500

    version 1

---
+++ singleton1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="singleton1.MvcApplication" Language="VB" %>
---
+++ singleton1/business/LabelSingleton.vb
@@ -0,0 +1,27 @@
+﻿Public Class LabelSingleton
+  
+    {
+    Private Static Singleton instance = null;
+    Private Static ReadOnly Object padlock = New Object();
+
+    Singleton()
+    {
+    }
+
+    Public Static Singleton Instance
+    {
+    Get
+    {
+    lock (padlock)
+    {
+    If (instance == null)
+    {
+    instance = New Singleton();
+    }
+    Return instance;
+    }
+    }
+    }
+    }
+
+End Class
